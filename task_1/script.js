2fda5cc4ec1fffb26241d43a2cb406aeee863e01
mdebrahim98 <mdebrahim742148@gmail.com>
2021-12-07 14:27:46 +0530

    task1 completed

---
+++ task_1/script.js
@@ -0,0 +1,55 @@
+let slideIndex = 1;
+const eventsImage = document.querySelector('.events .wrapper img');
+
+const sources =  ['event1.jpg','event2.jpg','event3.jpg' ];
+let currentImage  = 0;
+
+
+setInterval(() => {
+  if(currentImage<sources.length){
+    eventsImage.src = `./img/${sources[currentImage]}`;
+
+    currentImage++;
+   
+  }else if(currentImage>=sources.length){
+    currentImage = 0;
+    eventsImage.src = `./img/${sources[currentImage]}`;
+
+    currentImage++;
+    
+    
+  }
+ 
+
+  
+}, 3000);
+
+
+
+
+showSlides(slideIndex);
+
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
